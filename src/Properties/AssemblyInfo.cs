cb3a4b25444a5a7c6275b7f340d929356d2c7319
Stian Hanger <pdnagilum@gmail.com>
2021-03-09 14:50:16 +0100

    Fix some app info

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebConfig ConnectionString Tool")]
+[assembly: AssemblyTitle("SQL Command-Line Tool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebConfig ConnectionString Tool")]
-[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyProduct("SQL Command-Line Tool")]
+[assembly: AssemblyCopyright("Copyright © 2021 Stian Hanger")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
